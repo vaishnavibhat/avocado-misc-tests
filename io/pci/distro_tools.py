fbc349de6ff62fef6061555b87498719c75eb8b3
Vaishnavi Bhat <vaishnavi@linux.vnet.ibm.com>
2020-02-15 10:43:10 +0530

    adding more tools to distro_tools.py
    adding lsvio, lsdevinfo, usysident, usysattn, ofpathname
    to the list
    
    Signed-off-by: Vaishnavi Bhat <vaishnavi@linux.vnet.ibm.com>

--- io/pci/distro_tools.py
+++ io/pci/distro_tools.py
@@ -37,6 +37,8 @@ class DisrtoTool(Test):
         '''
         self.option = self.params.get("test_opt", default='').split(",")
         self.tool = self.params.get("tool", default='')
+        self.interface = self.params.get("interface", default='')
+        self.location_code = self.params.get("location_code", default='')
 
     @skipUnless(IS_POWER_VM,
                 "supported only on PowerVM platform")
@@ -74,6 +76,58 @@ class DisrtoTool(Test):
         if result.exit_status != 0:
             self.fail("lsprop failed")
 
+    def lsvio(self, option):
+        '''
+        run lsvio aaplicable only for PowerVM
+        '''
+        cmd = "lsvio -%s" % option
+        result = process.run(cmd, shell=True, ignore_status=True)
+        if result.exit_status != 0:
+            self.fail("lsvio failed")
+
+    def lsdevinfo(self, option):
+        '''
+        run lsdevinfo
+        '''
+        cmd = "lsdevinfo -%s" % option
+        result = process.run(cmd, shell=True, ignore_status=True)
+        if result.exit_status != 0:
+            self.fail("lsdevinfo failed")
+
+    def usys(self, tool, option, interface, location_code):
+        '''
+        run usysident
+        '''
+        if option == "-P -s identify -l":
+            cmd = "%s %s -l %s " % (tool, option, location_code)
+        elif option == "-t -s normal -d":
+            cmd = "%s %s -d %s " % (tool, option, interface)
+        elif option == "-P -s normal -l":
+            cmd = "%s %s -l %s " % (tool, option, location_code)
+        else:
+            cmd = "%s %s -d %s " % (tool, option, interface)
+
+        result = process.run(cmd, shell=True, ignore_status=True)
+        if result.exit_status != 0:
+            self.fail("%s option %s failed" % (tool, option))
+    
+    def usysattn(self, option, interface, location_code):
+        self.usys(self.tool, option, interface, location_code)
+        return
+ 
+    def usysident(self, option, interface, location_code):
+        self.usys(self.tool, option, interface, location_code)
+        return
+
+    def ofpathname(self, option):
+        '''
+        run ofpathname aaplicable only for PowerVM
+        '''
+        cmd = "ofpathname -%s" % option
+        result = process.run(cmd, shell=True, ignore_status=True)
+        if result.exit_status != 0:
+            self.fail("ofpathname failed")
+
     def test(self):
         '''
         test different distro tools
@@ -87,6 +141,22 @@ class DisrtoTool(Test):
         elif self.tool == "lsprop":
             for val in self.option:
                 self.lsprop(val)
+        elif self.tool == "lsvio":
+            for val in self.option:
+                self.lsvio(val)
+        elif self.tool == "lsdevinfo":
+            for val in self.option:
+                self.lsdevinfo(val)
+        elif self.tool == "usysident":
+            for val in self.option:
+                self.usysident(val, self.interface, self.location_code)
+        elif self.tool == "usysattn":
+            for val in self.option:
+                self.usysattn(val, self.interface, self.location_code)
+        elif self.tool == "ofpathname":
+            for val in self.option:
+                self.ofpathname(val)
+
 
 
 if __name__ == "__main__":
